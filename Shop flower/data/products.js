d8000179ac1b3e169c61aa33262e664c83cd178d
SYNGUYEN2k3 <143114249+SYNGUYEN2k3@users.noreply.github.com>
2024-04-23 11:54:41 +0700

    Add files via upload

---
+++ Shop flower/data/products.js
@@ -0,0 +1,404 @@
+var list_products = [{
+    "name": "Hoa hồng mắt",
+    "company": "Hoa hồng",
+    "img": "img/products/8.jpg",
+    "price": "3.490.000",
+    "star": 3,
+    "rateCount": 26,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+
+}, {
+    "name": "Xiaomi Mi 8 Lite",
+    "company": "Xiaomi",
+    "img": "img/products/xiaomi-mi-8-lite-black-1-600x600.jpg",
+    "price": "6.690.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+
+}, {
+    "name": "Hoa hồng cổ",
+    "company": "Hoa hồng",
+    "img": "https://icdn.dantri.com.vn/2020/10/30/9-x-dong-nai-lam-khu-vuon-hoa-hong-rong-2000-m-2-dep-nhu-co-tichdocx-1604062210548.jpeg",
+    "price": "7.690.000",
+    "star": 5,
+    "rateCount": 188,
+    "promo": {
+        "name": "giamgia",
+        "value": "500.000"
+    },
+
+},
+
+{
+    "name": "Samsung Galaxy A8+ (2018)",
+    "company": "Samsung",
+    "img": "img/products/9.jpg",
+    "price": "11.990.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "giamgia",
+        "value": "1.500.000"
+    },
+
+},
+
+
+{
+    "name": "Hoa mai",
+    "company": "Hoa mai",
+    "img": "img/products/11.jpg",
+    "price": "6.290.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "giamgia",
+        "value": "500.000"
+    },
+
+}, {
+    "name": "Hoa mai xuân",
+    "company": "Hoa mai",
+    "img": "img/products/12.jpg",
+    "price": "8.990.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+
+}, {
+    "name": "Hoa hồng xuân",
+    "company": "Hoa hồng",
+    "img": "img/products/10.jpg",
+    "price": "17.000.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "giareonline",
+        "value": "16.990.000"
+    },
+
+}, {
+    "name": "Xiaomi Mi 8",
+    "company": "Xiaomi",
+    "img": "img/products/xiaomi-mi-8-1-600x600.jpg",
+    "price": "12.990.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "",
+        "value": "0"
+    },
+
+},
+
+{
+    "name": "Hoa hồng biển",
+    "company": "Hoa hồng",
+    "img": "img/products/3.jpg",
+    "price": "2.850.000",
+    "star": 4,
+    "rateCount": 104,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+
+},
+{
+    "name": "Hoa hồng lãng mạng",
+    "company": "Hoa hồng",
+    "img": "img/products/5.jpg",
+    "price": "2.490.000",
+    "star": 4,
+    "rateCount": 80,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+
+}, {
+    "name": "Hoa hồng quế",
+    "company": "hoa hồng",
+    "img": "img/products/4.jpg",
+    "price": "260.000",
+    "star": 5,
+    "rateCount": 87,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+
+}, {
+    "name": "Hoa hồng Đỏ",
+    "company": "Hoa hồng",
+    "img": "img/products/1.jpg",
+    "price": "5.690.000",
+    "star": 4,
+    "rateCount": 372,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+
+}, {
+    "name": "Hoa hồng sữa",
+    "company": "Hoa hồng ",
+    "img": "img/products/2.jpg",
+    "price": "4.790.000",
+    "star": 4,
+    "rateCount": 347,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+
+},
+
+{
+    "name": "Hoa hồng lửa",
+    "company": "Hoa hồng",
+    "img": "img/products/9.jpg",
+    "price": "9.990.000",
+    "star": 4,
+    "rateCount": 22,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+
+}, {
+    "name": "Hoa hồng gai",
+    "company": "hoa hồng",
+    "img": "img/products/6.jpg",
+    "price": "1.990.000",
+    "star": 5,
+    "rateCount": 54,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+
+}, {
+    "name": "Hoa hồng ý tưởng",
+    "company": "Hoa hồng ý tưởng",
+    "img": "https://wall.vn/wp-content/uploads/2019/11/hinh-nen-hoa-hong-dep-17.jpg",
+    "price": "999.999.000",
+    "star": 5,
+    "rateCount": 9999,
+    "promo": {
+        "name": "giamgia",
+        "value": "1.000"
+    },
+
+}, {
+    "name": "Hoa hồng sơn",
+    "company": "Hoa hồng",
+    "img": "https://s1.media.ngoisao.vn/resize_580/news/2024/04/16/trong-hoa3-ngoisaovn-w600-h354.jpg",
+    "price": "8.990.000",
+    "star": 4,
+    "rateCount": 22,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+
+},
+
+{
+    "name": "Hoa hồng vàng",
+    "company": "Hoa hồng",
+    "img": "https://ohay.vn/blog/wp-content/uploads/2020/12/cac-loai-hoa-chung-ngay-tet-6.jpg",
+    "price": "2.390.000",
+    "star": 3,
+    "rateCount": 30,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+
+}, {
+    "name": "Hoa hồng xanh",
+    "company": "hoa hồng",
+    "img": "https://section8chicago.com/wp-content/uploads/2019/10/cac-loai-hoa-hong-13.jpg",
+    "price": "260.000",
+    "star": 5,
+    "rateCount": 126,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+
+}, {
+    "name": "Vivo V11",
+    "company": "Vivo",
+    "img": "https://cdn.tgdd.vn/Products/Images/42/188828/vivo-v11-600x600.jpg",
+    "price": "10.990.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+
+}, {
+    "name": "Hoa hồng rừng",
+    "company": "Hoa hồng",
+    "img": "https://hoahongsaigon.com/wp-content/uploads/2020/07/vuon-hoa-hong-co.jpg",
+    "price": "7.490.000",
+    "star": 5,
+    "rateCount": 168,
+    "promo": {
+        "name": "giamgia",
+        "value": "800.000"
+    },
+
+}, {
+    "name": "Hoa hồng xanh dương",
+    "company": "Hoa hồng",
+    "img": "https://www.hoatuoivn.net/wp-content/uploads/2019/12/y-nghia-cua-hoa-hong-xanh-duong-loai-hoa-doc-dao-3-e1577264993213.jpg",
+    "price": "4.990.000",
+    "star": 4,
+    "rateCount": 60,
+    "promo": {
+        "name": "giamgia",
+        "value": "500.000"
+    },
+
+}, {
+    "name": "Hoa hồng xưa",
+    "company": "Hoa hồng",
+    "img": "https://img2.thuthuatphanmem.vn/uploads/2019/01/29/hoa-hong-sempervirens-5_083646871.jpg",
+    "price": "3.290.000",
+    "star": 4,
+    "rateCount": 60,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+
+}, {
+    "name": "Hoa hồng súng",
+    "company": "hoa hồng",
+    "img": "https://tse3.mm.bing.net/th?id=OIP.NIAuovqbx6eaoKG24XSdigHaEo&pid=Api&P=0&h=220",
+    "price": "550.000",
+    "star": 4,
+    "rateCount": 37,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+
+}, {
+    "name": "Hoa hổng bỉ ngạn",
+    "company": "Mobell",
+    "img": "https://wall.vn/wp-content/uploads/2019/11/hinh-anh-hoa-bi-ngan-3-1536x864.jpg",
+    "price": "590.000",
+    "star": 4,
+    "rateCount": 58,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+
+}, {
+    "name": "Hoa cúc",
+    "company": "Hoa cúc",
+    "img": "https://3.bp.blogspot.com/-rLeZixCHerY/VmrFC4_POOI/AAAAAAAAAak/maosogBzLAw/s1600/anh-hoa-lan-1.jpg",
+    "price": "1.790.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+
+},
+
+{
+    "name": "Hoa hồng tương",
+    "company": "Hoa hồng",
+    "img": "http://hoisvcvn.org.vn/Uploads/images/Anh_HoahongTudor.jpg",
+    "price": "1.350.000",
+    "star": 5,
+    "rateCount": 3,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+
+},
+{
+    "name": "Hoa hồng bó",
+    "company": "Hoa hồng",
+    "img": "https://i.pinimg.com/originals/e4/50/aa/e450aa2bf91b572a2902691d3d1ddb77.jpg",
+    "price": "160.000",
+    "star": 5,
+    "rateCount": 302,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+
+}, {
+    "name": "Hoa hồng lửa",
+    "company": "Hoa hồng",
+    "img": "https://wall.vn/wp-content/uploads/2021/03/hinh-nen-ngay-8-3-dep-9.jpg",
+    "price": "170.000",
+    "star": 5,
+    "rateCount": 33,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+
+},
+
+{
+    "name": "Hoa Hồng Đẹp",
+    "company": "Hoa Hồng",
+    "img": "https://anphupet.com/wp-content/uploads/2021/01/cac-loai-hoa-hong-dep.jpg",
+    "price": "20.990.000",
+    "star": 5,
+    "rateCount": 230,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+
+}, {
+    "name": "Hoa hồng mũ",
+    "company": "Hoa hồng",
+    "img": "http://hoisvcvn.org.vn/Uploads/images/Anh_HoahongTudor.jpg",
+    "price": "25.790.000",
+    "star": 5,
+    "rateCount": 16,
+    "promo": {
+        "name": "giamgia",
+        "value": "500.000"
+    },
+
+},
+
+{
+    "name": "Hoa hồng lãng mạng",
+    "company": "Hoa hồng",
+    "img": "https://image-us.eva.vn/upload/1-2019/images/2019-03-27/cac-loai-hoa-hong-dep-tren-the-gioi-hoa-hong-dep-14-1553658990-166-width450height600.jpg",
+    "price": "4.490.000",
+    "star": 4,
+    "rateCount": 804,
+    "promo": {
+        "name": "giareonline",
+        "value": "3.990.000"
+    },
+
+}]
---
+++ Shop flower/js/dungchung.js
@@ -0,0 +1,402 @@
+var adminInfo = [{
+    "username": "admin",
+    "pass": "adadad"
+}];
+
+function getListAdmin() {
+    return JSON.parse(window.localStorage.getItem('ListAdmin'));
+}
+
+function setListAdmin(l) {
+    window.localStorage.setItem('ListAdmin', JSON.stringify(l));
+}
+
+
+// Hàm khởi tạo, tất cả các trang đều cần
+function khoiTao() {
+    // get data từ localstorage
+    list_products = getListProducts() || list_products;
+    adminInfo = getListAdmin() || adminInfo;
+
+    setupEventTaiKhoan();
+    capNhat_ThongTin_CurrentUser();
+    addEventCloseAlertButton();
+}
+
+// ========= Các hàm liên quan tới danh sách sản phẩm =========
+// Localstorage cho dssp: 'ListProducts
+function setListProducts(newList) {
+    window.localStorage.setItem('ListProducts', JSON.stringify(newList));
+}
+
+function getListProducts() {
+    return JSON.parse(window.localStorage.getItem('ListProducts'));
+}
+
+function timKiemTheoTen(list, ten, soluong) {
+    var tempList = copyObject(list);
+    var result = [];
+    ten = ten.split(' ');
+
+    for (var sp of tempList) {
+        var correct = true;
+        for (var t of ten) {
+            if (sp.name.toUpperCase().indexOf(t.toUpperCase()) < 0) {
+                correct = false;
+                break;
+            }
+        }
+        if (correct) {
+            result.push(sp);
+        }
+    }
+
+    return result;
+}
+
+function timKiemTheoMa(list, ma) {
+    for (var l of list) {
+        if (l.masp == ma) return l;
+    }
+}
+
+// copy 1 object, do trong js ko có tham biến , tham trị rõ ràng
+// nên dùng bản copy để chắc chắn ko ảnh hưởng tới bản chính
+function copyObject(o) {
+    return JSON.parse(JSON.stringify(o));
+}
+
+
+
+function addEventCloseAlertButton() {
+    document.getElementById('closebtn')
+        .addEventListener('mouseover', (event) => {
+            // event.target.parentElement.style.display = "none";
+            event.target.parentElement.style.opacity = 0;
+            event.target.parentElement.style.zIndex = 0;
+        });
+}
+
+
+function getCurrentUser() {
+    return JSON.parse(window.localStorage.getItem('CurrentUser')); // Lấy dữ liệu từ localstorage
+}
+
+function setCurrentUser(u) {
+    window.localStorage.setItem('CurrentUser', JSON.stringify(u));
+}
+
+
+
+// Tạo event, hiệu ứng cho form tài khoản
+function setupEventTaiKhoan() {
+    var taikhoan = document.getElementsByClassName('taikhoan')[0];
+    var list = taikhoan.getElementsByTagName('input');
+
+    // Tạo eventlistener cho input để tạo hiệu ứng label
+    // Gồm 2 event onblur, onfocus được áp dụng cho từng input trong list bên trên
+    ['blur', 'focus'].forEach(function (evt) {
+        for (var i = 0; i < list.length; i++) {
+            list[i].addEventListener(evt, function (e) {
+                var label = this.previousElementSibling; // lấy element ĐỨNG TRƯỚC this, this ở đây là input
+                if (e.type === 'blur') { // khi ấn chuột ra ngoài
+                    if (this.value === '') { // không có value trong input thì đưa label lại như cũ
+                        label.classList.remove('active');
+                        label.classList.remove('highlight');
+                    } else { // nếu có chữ thì chỉ tắt hightlight chứ không tắt active, active là dịch chuyển lên trên
+                        label.classList.remove('highlight');
+                    }
+                } else if (e.type === 'focus') { // khi focus thì label active + hightlight
+                    label.classList.add('active');
+                    label.classList.add('highlight');
+                }
+            });
+        }
+    })
+
+
+}
+
+// Cập nhật số lượng hàng trong giỏ hàng + Tên current user
+function capNhat_ThongTin_CurrentUser() {
+    var u = getCurrentUser();
+    if (u) {
+        // Cập nhật số lượng hàng vào header
+        document.getElementsByClassName('cart-number')[0].innerHTML = getTongSoLuongSanPhamTrongGioHang(u);
+        // Cập nhật tên người dùng
+        document.getElementsByClassName('member')[0]
+            .getElementsByTagName('a')[0].childNodes[2].nodeValue = ' ' + u.username;
+        // bỏ class hide của menu người dùng
+        document.getElementsByClassName('menuMember')[0]
+            .classList.remove('hide');
+    }
+}
+
+
+function autocomplete(inp, arr) {
+    var currentFocus;
+
+    inp.addEventListener("keyup", function (e) {
+        if (e.keyCode != 13 && e.keyCode != 40 && e.keyCode != 38) { // not Enter,Up,Down arrow
+            var a, b, i, val = this.value;
+
+            /*close any already open lists of autocompleted values*/
+            closeAllLists();
+            if (!val) {
+                return false;
+            }
+            currentFocus = -1;
+
+            /*create a DIV element that will contain the items (values):*/
+            a = document.createElement("DIV");
+            a.setAttribute("id", this.id + "autocomplete-list");
+            a.setAttribute("class", "autocomplete-items");
+
+            /*append the DIV element as a child of the autocomplete container:*/
+            this.parentNode.appendChild(a);
+
+            /*for each item in the array...*/
+            for (i = 0; i < arr.length; i++) {
+                /*check if the item starts with the same letters as the text field value:*/
+                if (arr[i].name.substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+
+                    /*create a DIV element for each matching element:*/
+                    b = document.createElement("DIV");
+
+                    /*make the matching letters bold:*/
+                    b.innerHTML = "<strong>" + arr[i].name.substr(0, val.length) + "</strong>";
+                    b.innerHTML += arr[i].name.substr(val.length);
+
+                    /*insert a input field that will hold the current array item's value:*/
+                    b.innerHTML += "<input type='hidden' value='" + arr[i].name + "'>";
+
+                    /*execute a function when someone clicks on the item value (DIV element):*/
+                    b.addEventListener("click", function (e) {
+                        /*insert the value for the autocomplete text field:*/
+                        inp.value = this.getElementsByTagName("input")[0].value;
+                        inp.focus();
+
+                        /*close the list of autocompleted values,
+                        (or any other open lists of autocompleted values:*/
+                        closeAllLists();
+                    });
+                    a.appendChild(b);
+                }
+            }
+        }
+
+    });
+    /*execute a function presses a key on the keyboard:*/
+    inp.addEventListener("keydown", function (e) {
+        var x = document.getElementById(this.id + "autocomplete-list");
+        if (x) x = x.getElementsByTagName("div");
+        if (e.keyCode == 40) {
+            /*If the arrow DOWN key is pressed, increase the currentFocus variable:*/
+            currentFocus++;
+            /*and and make the current item more visible:*/
+            addActive(x);
+        } else if (e.keyCode == 38) { //up
+            /*If the arrow UP key is pressed,
+            decrease the currentFocus variable:*/
+            currentFocus--;
+            /*and and make the current item more visible:*/
+            addActive(x);
+        } else if (e.keyCode == 13) {
+            /*If the ENTER key is pressed, prevent the form from being submitted,*/
+
+            if (currentFocus > -1) {
+                /*and simulate a click on the "active" item:*/
+                if (x) {
+                    x[currentFocus].click();
+                    e.preventDefault();
+                }
+            }
+        }
+    });
+
+
+}
+
+// Thêm từ khóa tìm kiếm
+function addTags(nameTag, link) {
+    var new_tag = `<a href=` + link + `>` + nameTag + `</a>`;
+
+    // Thêm <a> vừa tạo vào khung tìm kiếm
+    var khung_tags = document.getElementsByClassName('tags')[0];
+    khung_tags.innerHTML += new_tag;
+}
+
+// Thêm sản phẩm vào trang
+function addProduct(p, ele, returnString) {
+    promo = new Promo(p.promo.name, p.promo.value); // class Promo
+    product = new Product(p.masp, p.name, p.img, p.price, p.star, p.rateCount, promo); // Class product
+
+    return addToWeb(product, ele, returnString);
+}
+
+
+
+// Thêm header
+function addHeader() {
+    document.write(`        
+	<div class="header group">
+        <div class="logo">
+            <a href="index.html">
+                <img src="img/logo.jpg" alt="Trang chủ Smartphone Store" title="Trang chủ Smartphone Store">
+            </a>
+        </div> <!-- End Logo -->
+
+        <div class="content">
+            <div class="search-header" style="position: relative; left: 50px; top: 1px;">
+                <form class="input-search" method="get" action="index.html">
+                    <div class="autocomplete">
+                        <input id="search-box" name="search" autocomplete="off" type="text" placeholder="Bạn cần tìm kiếm gì...">
+                        <button type="submit">
+                            <i class="fa fa-search"></i>
+                            TÌM KIẾM
+                        </button>
+                    </div>
+                </form> <!-- End Form search -->
+                <div class="tags">
+                    <strong>Gợi Ý: </strong>
+                </div>
+            </div>                     <!-- End Search header -->
+
+            <div class="tools-member">
+                <div class="member">
+                    <a onclick="checkTaiKhoan()">
+                        <i class="fa fa-user"></i>
+                        Tài khoản
+                    </a>
+                    <div class="menuMember hide">
+                        <a href="nguoidung.html">Trang khách hàng</a>
+                        <a onclick="if(window.confirm('Xác nhận đăng xuất ?')) logOut();">Đăng xuất</a>
+                    </div>
+
+                </div> <!-- End Member -->
+
+                <div class="cart">
+                    <a href="giohang.html">
+                        <i class="fa fa-shopping-cart"></i>
+                        <span>Giỏ hàng</span>
+                        <span class="cart-number"></span>
+                    </a>
+                </div> <!-- End Cart -->
+
+                <!--<div class="check-order">
+                    <a>
+                        <i class="fa fa-truck"></i>
+                        <span>Đơn hàng</span>
+                    </a>
+                </div> -->
+            </div><!-- End Tools Member -->
+        </div> <!-- End Content -->
+    </div> <!-- End Header -->`)
+}
+
+function addFooter() {
+    document.write(`
+    <!-- ============== Alert Box ============= -->
+    <div id="alert">
+        <span id="closebtn">&otimes;</span>
+    </div>
+
+    <!-- ============== Footer ============= -->
+    <div class="copy-right">
+        <p><a href="index.html">Cửa hàng hoa tươi uy tín nhất 2024</a> - Địa chỉ: 377 Tôn Đức Thắng, phường Hòa Khánh Nam, quận Liên Chiểu, thành phố Đà Nẵng - Hotline: 0864842599, Email: Cuahang2024@gmail.com - Thiết kế by
+            
+    </div>`);
+}
+
+// Thêm contain Taikhoan
+function addContainTaiKhoan() {
+    document.write(`
+	<div class="containTaikhoan">
+        <span class="close" onclick="showTaiKhoan(false);">&times;</span>
+        <div class="taikhoan">
+
+            <ul class="tab-group">
+                <li class="tab active"><a href="#login">Đăng nhập</a></li>
+                <li class="tab"><a href="#signup">Đăng kí</a></li>
+            </ul> <!-- /tab group -->
+
+            <div class="tab-content">
+                <div id="login">
+                    <h1>Chào mừng bạn trở lại!</h1>
+
+                    <form onsubmit="return logIn(this);">
+
+                        <div class="field-wrap">
+                            <label>
+                                Tên đăng nhập<span class="req">*</span>
+                            </label>
+                            <input name='username' type="text" required autocomplete="off" />
+                        </div> <!-- /user name -->
+
+                        <div class="field-wrap">
+                            <label>
+                                Mật khẩu<span class="req">*</span>
+                            </label>
+                            <input name="pass" type="password" required autocomplete="off" />
+                        </div> <!-- pass -->
+
+                        <p class="forgot"><a href="#">Quên mật khẩu?</a></p>
+
+                        <button type="submit" class="button button-block" />Tiếp tục</button>
+
+                    </form> <!-- /form -->
+
+                </div> <!-- /log in -->
+
+                <div id="signup">
+                    <h1>Đăng kí miễn phí</h1>
+
+                    <form onsubmit="return signUp(this);">
+
+                        <div class="top-row">
+                            <div class="field-wrap">
+                                <label>
+                                    Họ<span class="req">*</span>
+                                </label>
+                                <input name="ho" type="text" required autocomplete="off" />
+                            </div>
+
+                            <div class="field-wrap">
+                                <label>
+                                    Tên<span class="req">*</span>
+                                </label>
+                                <input name="ten" type="text" required autocomplete="off" />
+                            </div>
+                        </div> <!-- / ho ten -->
+
+                        <div class="field-wrap">
+                            <label>
+                                Địa chỉ Email<span class="req">*</span>
+                            </label>
+                            <input name="email" type="email" required autocomplete="off" />
+                        </div> <!-- /email -->
+
+                        <div class="field-wrap">
+                            <label>
+                                Tên đăng nhập<span class="req">*</span>
+                            </label>
+                            <input name="newUser" type="text" required autocomplete="off" />
+                        </div> <!-- /user name -->
+
+                        <div class="field-wrap">
+                            <label>
+                                Mật khẩu<span class="req">*</span>
+                            </label>
+                            <input name="newPass" type="password" required autocomplete="off" />
+                        </div> <!-- /pass -->
+
+                        <button type="submit" class="button button-block" />Tạo tài khoản</button>
+
+                    </form> <!-- /form -->
+
+                </div> <!-- /sign up -->
+            </div><!-- tab-content -->
+
+        </div> <!-- /taikhoan -->
+    </div>`);
+}
